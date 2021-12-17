6ffa8ba89830f8d846b0079be9c7a4cf1fd389b7
Ali Yaman <aliymn.db@gmail.com>
2021-12-18 02:00:29 +0300

    lexical

--- LexicalAnalyzer.h
+++ LexicalAnalyzer.h
@@ -6,21 +6,24 @@ using namespace std;
 
 class LexicalAnalyzer{
   public:
+    // lexical analysis
     void getFileName(ifstream &);
     void scanFile(ifstream &);
     void analyzeToken(vector<char>);
     bool isFunction(string); // class, void, int, float, double etc.
     bool isStatement(string); // if , while , for, switch...
+    bool isLib(string); // #include
     bool isAssignmentOperator(string); //  +,=,/,* ...
-    bool isParenL(string); // (
-    bool isParenR(string); // )
-    bool isCurlL(string); // {
-    bool isCurlR(string); // }
-    bool isSemicolon(string);// ;
+    bool isParenL(char); // (
+    bool isParenR(char); // )
+    bool isCurlL(char); // {
+    bool isCurlR(char); // }
+    bool isSemicolon(char);// ;
     bool isComment(string); //  /* */ , //
     bool isWhiteSpace(char); // calculating all spaces.
     bool isSymbol(char); // calculating all symbols.
     bool isVar(string); // variables int,double,char,string,bool...
+    bool isValid(string); // variables int,double,char,string,bool...
   private:
     vector<char> token;
 };
--- lex.cpp
+++ lex.cpp
@@ -6,6 +6,20 @@
 
 using namespace std;
 
+  // calculate global variables
+  int comments = 0; 
+  int variables = 0;
+  int function_classes = 0;
+  int statements = 0;
+  int libs = 0;
+  int open_parent = 0;
+  int off_parent = 0;
+  int open_curly = 0;
+  int off_curly = 0;
+  int semicolon = 0;
+  int lines = 0;
+  int total = 0;
+
 int main()
 {
   LexicalAnalyzer lexicalAnalyzer;
@@ -17,17 +31,18 @@ int main()
   return 0;
 }
 
+
 // allows user to enter a file name to open
 void LexicalAnalyzer::getFileName(ifstream &myFile)
 {
-  string fileName;
+  string fileName = "input.txt";
 
-  cout << "Enter file name: ";
+  // cout << "Enter file name: ";
 
   // keep prompting until user has typed in a valid file name
   while (true)
   {
-    cin >> fileName;
+    // cin >> fileName;
     myFile.open(fileName);
 
     if (!myFile)
@@ -51,18 +66,19 @@ void LexicalAnalyzer::scanFile(ifstream &myFile)
   string myString = "";
   int tempX;
 
+
   // continue to go through input file line by line
   while (getline(myFile, readLine))
   {
+    lines = lines +1;
     // parse line to get tokens
-    for (int x = 0; x < readLine.length(); x++)
+    for (int x = 0; x <= readLine.length(); x++)
     {
       tempX = x;
       lookahead = readLine[x];
 
       // add character to vector to keep track of token
       token.push_back(readLine[x]);
-
       // analyze token since white space is read
       if (isWhiteSpace(lookahead))
       {
@@ -70,9 +86,65 @@ void LexicalAnalyzer::scanFile(ifstream &myFile)
         analyzeToken(token);
         token.clear();
       }
+
+      // checks if token is a left parentesis
+      if (isParenL(readLine[x]))
+      {
+        // cout << "Open parenthesis :            " << readLine[x] << endl;
+      }
+
+      if (isParenR(readLine[x]))
+      {
+        // cout << "Closed parenthesis :            " << readLine[x] << endl;
+      }
+
+      // checks if token is left curly
+      if (isCurlL(readLine[x]))
+      {
+        // cout << "Open curly braces :             " << readLine[x] << endl;
+      }
+
+      // checks if token is right curly
+      if (isCurlR(readLine[x]))
+      {
+        // cout << "Closed curly braces :             " << readLine[x] << endl;
+      }
+      
+      // checks if token is a semicolon
+      if (isSemicolon(readLine[x]))
+      {
+        // cout << "Semicolons :    " << readLine[x] << endl;
+      }
+
     }
   }
+
+  // display all calculates.
+  cout << "### Lexical Analysis Starting... ###" << endl;
+  cout << "Comments : " << comments << endl;
+  cout << "Variables : " << variables << endl;
+  cout << "Function or Classes : " << function_classes << endl;
+  cout << "Statements : " << statements << endl;
+  cout << "Modules : " << libs << endl;
+  cout << "open parenthesis : " << open_parent << endl;
+  cout << "open curly braces : " << open_curly << endl;
+  cout << "semicolon : " << semicolon << endl;
+  cout << "lines : " << lines << endl;
+  total = comments + variables + function_classes + statements + libs + open_parent + open_curly + semicolon + lines;
+  cout << "totals : " << total << endl;
+  if ((off_curly == open_curly) && (off_parent == open_parent)){
+    cout << "Bracket balance successful!" << endl;
+  }
+  else{
+    cout << "Bracket balance unsuccessful!" << endl;
+  }
+  cout << "### Lexical Analysis Ending... ###" << endl;
+
+  // cout << "### Syntax Analysis Starting... ###" << endl;
+  // cout << "### Syntax Analysis Ending... ###" << endl;
+
 }
+
 
 //******************************************************************************
 
@@ -89,12 +161,12 @@ bool LexicalAnalyzer::isWhiteSpace(char ch)
   }
 }
 
-// returns true if argument is a symbol
-bool LexicalAnalyzer::isSymbol(char ch)
+// returns true if argument is a FUNCTION keyword
+bool LexicalAnalyzer::isFunction(string currentToken)
 {
-  if (ch == '(' || ch == ')' || ch == ',' || ch == '{' || ch == '}' || ch == '+' ||
-      ch == '-' || ch == '*' || ch == '/' || ch == '&' || ch == '!' || ch == '|' || ch == ';')
+  if (currentToken == "class" || currentToken == "void" || currentToken == "main()" || currentToken == "main")
   {
+    function_classes = function_classes +1;
     return true;
   }
   else
@@ -103,11 +175,12 @@ bool LexicalAnalyzer::isSymbol(char ch)
   }
 }
 
-// returns true if argument is a FUNCTION keyword
-bool LexicalAnalyzer::isFunction(string currentToken)
+// returns true if argument is a IF keyword
+bool LexicalAnalyzer::isLib(string currentToken)
 {
-  if (currentToken == "class" || currentToken == "void" || currentToken == "main()" || currentToken == "main")
+  if (currentToken[0] == '#')
   {
+    libs = libs +1;
     return true;
   }
   else
@@ -121,6 +194,7 @@ bool LexicalAnalyzer::isStatement(string currentToken)
 {
   if (currentToken == "if" || currentToken == "else" || currentToken == "while" || currentToken == "for" || currentToken == "switch")
   {
+    statements = statements +1;
     return true;
   }
   else
@@ -129,11 +203,12 @@ bool LexicalAnalyzer::isStatement(string currentToken)
   }
 }
 
-// returns true if argument is a VAR keyword
+// returns true if argument is a variable keyword
 bool LexicalAnalyzer::isVar(string currentToken)
 {
   if (currentToken == "int" || currentToken == "float" || currentToken == "long" || currentToken == "double" || currentToken == "char" || currentToken == "string" || currentToken == "bool" || currentToken == "signed" || currentToken == "unsigned")
   {
+    variables = variables + 1;
     return true;
   }
   else
@@ -143,10 +218,11 @@ bool LexicalAnalyzer::isVar(string currentToken)
 }
 
 // returns true if argument is a left parentesis
-bool LexicalAnalyzer::isParenL(string currentToken)
+bool LexicalAnalyzer::isParenL(char currentToken)
 {
-  if (currentToken == "(")
+  if (currentToken == '(')
   {
+    open_parent = open_parent + 1;
     return true;
   }
   else
@@ -156,10 +232,11 @@ bool LexicalAnalyzer::isParenL(string currentToken)
 }
 
 // returns true if argument is a right parentesis
-bool LexicalAnalyzer::isParenR(string currentToken)
+bool LexicalAnalyzer::isParenR(char currentToken)
 {
-  if (currentToken == ")")
+  if (currentToken == ')')
   {
+    off_parent = off_parent +1;
     return true;
   }
   else
@@ -169,10 +246,11 @@ bool LexicalAnalyzer::isParenR(string currentToken)
 }
 
 // returns true if argument is a left curly brace
-bool LexicalAnalyzer::isCurlL(string currentToken)
+bool LexicalAnalyzer::isCurlL(char currentToken)
 {
-  if (currentToken == "{")
+  if (currentToken == '{')
   {
+    open_curly = open_curly +1;
     return true;
   }
   else
@@ -182,10 +260,11 @@ bool LexicalAnalyzer::isCurlL(string currentToken)
 }
 
 // returns true if argument is a right curly brace
-bool LexicalAnalyzer::isCurlR(string currentToken)
+bool LexicalAnalyzer::isCurlR(char currentToken)
 {
-  if (currentToken == "}")
+  if (currentToken == '}')
   {
+    off_curly = off_curly + 1;
     return true;
   }
   else
@@ -195,10 +274,11 @@ bool LexicalAnalyzer::isCurlR(string currentToken)
 }
 
 // returns true if argument is a SEMICOLON
-bool LexicalAnalyzer::isSemicolon(string currentToken)
+bool LexicalAnalyzer::isSemicolon(char currentToken)
 {
-  if (currentToken == ";")
+  if (currentToken == ';')
   {
+    semicolon = semicolon +1;
     return true;
   }
   else
@@ -225,6 +305,7 @@ bool LexicalAnalyzer::isComment(string currentToken)
 {
   if (currentToken == "//" || currentToken == "/*")
   {
+    comments = comments + 1;
     return true;
   }
   else
@@ -232,6 +313,7 @@ bool LexicalAnalyzer::isComment(string currentToken)
     return false;
   }
 }
+
 
 // figures out token type of argument
 void LexicalAnalyzer::analyzeToken(vector<char> token)
@@ -241,61 +323,40 @@ void LexicalAnalyzer::analyzeToken(vector<char> token)
   {
     currentToken += token[x];
   }
-
   // checks if token is a function keyword
   if (isComment(currentToken))
   {
-    cout << "Comments:         " << currentToken << endl;
+    // cout << "Comments:         " << currentToken << endl;
   }
   // checks if token is a function keyword
   else if (isFunction(currentToken))
   {
-    cout << "Functions or Classes :         " << currentToken << endl;
+    // cout << "Functions or Classes :         " << currentToken << endl;
   }
 
   else if (isAssignmentOperator(currentToken))
   {
-    cout << "Assignment Operators :         " << currentToken << endl;
+    // cout << "Assignment Operators :         " << currentToken << endl;
   }
 
   // checks if token is a if keyword
   else if (isStatement(currentToken))
   {
-    cout << "Statements :               " << currentToken << endl;
+    // cout << "Statements :               " << currentToken << endl;
   }
 
   // checks if token is a VAR keyword
   else if (isVar(currentToken))
   {
-    cout << "Variables :             " << currentToken << endl;
+    // cout << "Variables :             " << currentToken << endl;
   }
 
-  // checks if token is a semicolon
-  else if (isSemicolon(currentToken))
+  // checks if token is lib
+  else if (isLib(currentToken))
   {
-    cout << "Semicolons :    " << currentToken << endl;
-  }
-
-  // checks if token is a left parentesis
-  else if (isParenL(currentToken))
-  {
-    cout << "Open parenthesis :            " << currentToken << endl;
-  }
-
-  else if (isParenR(currentToken))
-  {
-    cout << "Closed parenthesis :            " << currentToken << endl;
-  }
-
-  // checks if token is left curly
-  else if (isCurlL(currentToken))
-  {
-    cout << "Open curly braces :             " << currentToken << endl;
-  }
-
-  // checks if token is right curly
-  else if (isCurlR(currentToken))
-  {
-    cout << "Closed curly braces :             " << currentToken << endl;
+    // cout << "Libs :             " << currentToken << endl;
   }
 }
+
+
+
